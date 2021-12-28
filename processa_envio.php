b19873cf70af1522b98953843406cc1d53ae8b8f
ElisaLissandra <elisa_lissandra@outlook.com>
2021-12-28 11:39:07 -0300

    instanciando a classe Mensagem

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,41 @@
 <?php
 
-  print_r($_POST);
-  
+  //print_r($_POST);
+
+  class Mensagem {
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function __get($atributo) {
+      return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+      $this->$atributo = $valor;
+    }
+
+    public function mensagemValida() {
+      if(empty($this->para) || empty($this->assunto)|| empty($this->mensagem)){
+        return false;
+      }
+
+      return true;
+
+    }
+  }
+
+  $mensagem = new Mensagem();
+  $mensagem->__set('para', $_POST['para']);
+  $mensagem->__set('assunto', $_POST['assunto']);
+  $mensagem->__set('mensagem', $_POST['mensagem']);
+
+  //print_r($mensagem);
+
+  if($mensagem->mensagemValida()){
+    echo 'Mensagem é válida';
+  } else {
+    echo 'Mensagem não é válida';
+  }
+
 ?>
