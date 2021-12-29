5c6eb77a9945b1d00e052c0a200f75bf9f80cdb8
ElisaLissandra <elisa_lissandra@outlook.com>
2021-12-28 21:06:26 -0300

    Enviando dado

--- processa_envio.php
+++ processa_envio.php
@@ -53,34 +53,34 @@
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
+    $mail->Username   = 'testecursos21@gmail.com';                     //SMTP username
+    $mail->Password   = 'curso2107';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->setFrom('testecursos21@gmail.com', 'Teste Curso Remetente');
+    $mail->addAddress('testecursos21@gmail.com', 'Teste Curso Destinatário');     //Add a recipient
     $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Oi. Eu sou o assunto';
+    $mail->Body    = 'Oi. Eu sou o conteúdo <strong>e-mail</strong>.';
+    $mail->AltBody = 'Oi. Eu sou o conteúdo e-mail.';
 
     $mail->send();
-      echo 'Não foi possível enviar este e-mail! Por favor novamente mais tarde.';
+      echo 'Mensagem enviada com sucesso';
   } catch (Exception $e) {
+    echo 'Não foi possível enviar este e-mail! Por favor novamente mais tarde.';
     echo "Detalhes do erro: {$mail->ErrorInfo}";
   }
 
